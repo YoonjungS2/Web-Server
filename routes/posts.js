88f02f741b6b90a4ed2f076fe8e3c53b2deeb6f8
YoonjungS2 <dbswjd9857@inha.edu>
2023-11-25 18:23:43 +0900

    ver 13

---
+++ routes/posts.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res){
+    res.render('index', {title:'게시판', pageName:'posts/list.ejs'})
+});
+
+router.get('/insert', function(req, res){
+    res.render('index', {title:'글쓰기', pageName:'posts/insert.ejs'})
+});
+
+router.get('/read', function(req, res){
+    const id=req.query.id;
+    res.render('index', {title:'게시글 정보', pageName:'posts/read.ejs', id})
+});
+
+router.get('/update', function(req, res){
+    const id=req.query.id;
+    res.render('index', {title:'게시글 수정', pageName:'posts/update.ejs', id})
+});
+
+module.exports = router;
